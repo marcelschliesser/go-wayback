ec3139b83ddb43f677ee0c53eba74bae9f938533
marcelschliesser <ms@thinq.digital>
2023-07-22 11:08:20 +0200

    Changes

--- server.go
+++ server.go
@@ -4,18 +4,31 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
-		data, err := ioutil.ReadFile("data.json")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(data)
-	})
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// Create a data instance with the message to be displayed in the template.
+	data, err := ioutil.ReadFile("data.json")
+	checkError(err)
+	// Parse the template file(s).
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+
+	// Execute the template with the provided data.
+	err2 := tmpl.Execute(w, string(data))
+	if err2 != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8080", nil)
 }

3300cbc5d82b451566f0d6f9302c567265ad6505
marcelschliesser <ms@thinq.digital>
2023-08-07 22:40:46 +0200

    PubSUb

--- preprocessor/src/main.go
+++ preprocessor/src/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"time"
+
+	"cloud.google.com/go/pubsub"
 )
 
 type Job struct {
@@ -33,11 +38,45 @@ func indexPage(w http.ResponseWriter, r *http.Request) {
 		j.Year = r.FormValue("year")
 		j.Email = r.FormValue("email")
 		j.OrderedAt = time.Now()
+
+		msgID, err := PublishMessage("go-wayback", "test-topic", j)
+		checkError(err)
+		log.Printf("Message published with ID: %s\n", msgID)
+
 		tmpl := template.Must(template.ParseFiles("templates/base.html", "templates/result.html"))
-		err := tmpl.ExecuteTemplate(w, "base.html", j)
+		err = tmpl.ExecuteTemplate(w, "base.html", j)
 		checkError(err)
 	}
 
+}
+
+// PublishMessage sends a message to a specified Google Cloud Pub/Sub topic.
+func PublishMessage(projectID, topicName string, data Job) (string, error) {
+	ctx := context.Background()
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return "", fmt.Errorf("failed to marshal data: %v", err)
+	}
+
+	client, err := pubsub.NewClient(ctx, projectID)
+	if err != nil {
+		return "", fmt.Errorf("failed to create client: %v", err)
+	}
+	defer client.Close()
+
+	topic := client.Topic(topicName)
+
+	msg := &pubsub.Message{
+		Data: jsonData,
+	}
+
+	msgID, err := topic.Publish(ctx, msg).Get(ctx)
+	if err != nil {
+		return "", fmt.Errorf("failed to publish message: %v", err)
+	}
+
+	return msgID, nil
 }
 
 func main() {
